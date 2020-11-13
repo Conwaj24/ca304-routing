a5629e4f3764c3aac4fc424a478b3cdb59fc2de8
JCoMcL <rapcyss@gmail>
2020-11-13 10:41:52 +0000

    add initialization logic for Route

--- classes/route.py
+++ classes/route.py
@@ -1,25 +1,39 @@
 #!/bin/env python
 
 class Route(object):
-    def __init__():
-        self.routers = []
-        self.cost = 0
+    def __init__(self, *routers_in_path, cost=None):
+        self.path = []
+        if cost:
+            self.path=routers_in_path
+            self.cost = cost
+        else:
+            self.calculate(routers_in_path)
+        self.is_edge = len(self.path) == 1
+
+    def calculate(self, *routers_in_path):
+        if len(routers_in_path) > 1:
+            self += (
+                    routers_in_path[0].get_path(routers_in_path[1]) +
+                    Route(routers_in_path[1:])
+            )
 
     def invert(self):
-        self.routers = self.routers[::-1]
+        self.path = self.path[::-1]
 
-    def getCost(arg=None):
+    def get_cost(self, arg=None):
         if arg is None:
             return self.cost
         if arg is int:
             return
 
     def __str__(self):
-        return "->".join(self.routers)
+        return "->".join(self.path)
     def __repr__(self):
         return str(self)
     def __int__(self):
         return self.cost
+    def __len__(self):
+        return len(self.path) + 1
 
 def main():
     pass
@@ -27,4 +41,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
